81a67bf7a03342587ab21bfa6e73394c62f9f2e3
Guillermo Leyva Castañón <42184356+memoley@users.noreply.github.com>
2024-10-28 12:45:16 -0600

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind()],
 
   env: {
